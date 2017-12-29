0e026df227f9a3329fcb81e0e802e491dfd3bb4f
mazenelmasry <mazenalex@gmail.com>
2017-12-30 01:59:46 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'magentotwotranslations_ar_sa',
+    'magentotwotranslations_ar_eg',
     __DIR__
 );
